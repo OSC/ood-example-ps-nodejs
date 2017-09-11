10e6ad66f3bee2202173f329a54c251fd61a04f5
Eric Franz <efranz@osc.edu>
2017-09-11 13:19:53 -0400

    convert to express app

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // a minimal node app that displays your running processes on the OOD web host
 var http = require('http');
 var exec = require('child_process').exec;
+var express   = require('express');
+var hbs       = require('hbs');
+var path      = require('path');
 
-var server = http.createServer(function (request, response){
-    exec('ps ufx', function(error, stdout, stderr){
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("<h2>" + new Date() + "</h2>");
-        response.write("<pre>" + stdout + "</pre>\n");
-        response.end();
-    });
+
+// create routes
+var router = express.Router();
+
+router.get("/", function(request, response){
+  exec('ps ufx', function(error, stdout, stderr){
+    response.send("<h2>" + new Date() + "</h2>\n<pre>" + stdout + "</pre>\n");
+  });
 });
-server.listen(3000);
 
+
+var app = express();
+
+// Setup template engine
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(process.env.PASSENGER_BASE_URI || '/', router);
+
+app.listen(3000);
